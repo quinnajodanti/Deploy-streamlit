e004ef701be3477602683ccdad977c496f5bfe43
anniuqitnadoj <44369692+quinnajodanti@users.noreply.github.com>
2021-12-22 15:20:54 +0700

    Update diabetes prediction with web.py

--- diabetes prediction with web.py
+++ diabetes prediction with web.py
@@ -9,7 +9,7 @@ import pickle
 import streamlit as st
 
 #loading the saved model
-loaded_model = pickle.load(open('D:/Downloads/trained_model.sav','rb'))
+loaded_model = pickle.load(open('trained_model.sav','rb'))
 
 #creating a function for prediction
 def diabetes_prediction(input_data):
